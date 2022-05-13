d4d627d1af9a7dad9648ece420da45d13a790dd9
Charm8er <josh@luttmer.us>
2022-05-12 22:02:47 -0600

    Initial Commit

---
+++ src/main/java/com/calibermc/terradornmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.calibermc.terradornmod.mixin;
+
+import com.calibermc.terradornmod.TerradornMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TerradornMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
